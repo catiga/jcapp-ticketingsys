83ff6f9ac050dbd3998b5c32a0c99e302a6f772b
jackielee <jackielee@192.168.1.3>
2019-06-04 18:38:29 +0800

    修改入场内部接口，支持按照传入参数决定是否修改座位入场状态

--- src/main/java/com/jeancoder/ticketingsys/internal/ticketing/take_by_code_for_direct_check.groovy
+++ src/main/java/com/jeancoder/ticketingsys/internal/ticketing/take_by_code_for_direct_check.groovy
@@ -1,19 +1,14 @@
 package com.jeancoder.ticketingsys.internal.ticketing
 
-import java.net.URLDecoder
-import java.util.List
+import java.sql.Timestamp
 
 import com.jeancoder.app.sdk.JC
 import com.jeancoder.app.sdk.source.LoggerSource
 import com.jeancoder.core.log.JCLogger
-import com.jeancoder.core.log.JCLoggerFactory
-import com.jeancoder.core.util.JackSonBeanMapper
 import com.jeancoder.jdbc.JcTemplate
 import com.jeancoder.ticketingsys.ready.dto.SimpleAjax
 import com.jeancoder.ticketingsys.ready.entity.SaleOrder
 import com.jeancoder.ticketingsys.ready.entity.SaleSeat
-import com.jeancoder.ticketingsys.ready.support.MoneyUtil
-import com.jeancoder.ticketingsys.ready.util.DateUtil
 
 // order_id, order_seat_id 这样入参
 JCLogger Logger = LoggerSource.getLogger();
@@ -24,6 +19,8 @@ if(!get_code) {
 get_code = get_code.split(',');
 def order_id = get_code[0];
 def order_seat_id = get_code[1];
+
+def modify_status = JC.internal.param('modify_status')?.toString();
 
 def sql = 'select * from SaleSeat where id=?';
 SaleSeat seat_info = JcTemplate.INSTANCE().get(SaleSeat, sql, order_seat_id);
@@ -44,7 +41,11 @@ if(order.order_status.startsWith('0')) {
 if(seat_info.went_status!='00') {
 	return SimpleAjax.notAvailable('status_invalid,该座位已经入场');
 }
-seat_info.went_status = '10';
-JcTemplate.INSTANCE().update(seat_info);
+if(modify_status && modify_status.equals('modify')) {
+	//修改为已入场状态
+	seat_info.went_status = '10';
+	seat_info.c_time = new Timestamp(Calendar.getInstance().getTimeInMillis());
+	JcTemplate.INSTANCE().update(seat_info);
+}
 
 return SimpleAjax.available('', [order, seat_info]);
