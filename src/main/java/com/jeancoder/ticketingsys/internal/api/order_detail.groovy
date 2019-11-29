1fe38255a1b0b36c0c496539fce83cea64372064
jackielee <jackielee@bogon>
2019-11-29 11:30:41 +0800

    c

--- src/main/java/com/jeancoder/ticketingsys/internal/api/order_detail.groovy
+++ src/main/java/com/jeancoder/ticketingsys/internal/api/order_detail.groovy
@@ -6,6 +6,7 @@ import com.jeancoder.app.sdk.source.ResponseSource
 import com.jeancoder.core.http.JCRequest
 import com.jeancoder.core.http.JCResponse
 import com.jeancoder.core.log.JCLogger
+import com.jeancoder.core.log.JCLoggerFactory
 import com.jeancoder.core.result.Result
 import com.jeancoder.jdbc.JcTemplate
 import com.jeancoder.ticketingsys.ready.dto.SimpleAjax
@@ -22,7 +23,7 @@ import com.jeancoder.ticketingsys.ready.support.Res
 JCRequest req = RequestSource.getRequest();
 JCResponse res = ResponseSource.getResponse();
 Result result = new Result();
-JCLogger LOGGER = LoggerSource.getLogger();
+JCLogger LOGGER = JCLoggerFactory.getLogger('order_detail');
 
 String order_no = JC.internal.param("order_no");
 def coupon_id = JC.internal.param('coupon_id');
@@ -31,6 +32,7 @@ def market_id = JC.internal.param('market_id');
 def mobile = JC.internal.param('mobile');
 def ap_id = JC.internal.param('ap_id');
 
+LOGGER.info('coupon_id=' + coupon_id + ', market_id=' + market_id);
 try {
 	OrderWithTicketDto order = OrderService.INSTANCE.getOrderWithTicketByNo(order_no);
 	if(order == null) {
@@ -44,6 +46,7 @@ try {
 	store_id = cinema.store_basic;
 	def pref_data = null;
 	if(market_id) {
+		LOGGER.info('join market');
 		//首先判断是否选择了活动
 		//活动逻辑先行
 		List<OrderSeatItemDto> items = OrderService.INSTANCE.getOrderSeats(order.id);
@@ -67,7 +70,7 @@ try {
 		pref_data = ret_obj;
 	} else if(coupon_id) {
 		//没有活动的情况，判断是否选择了券
-		
+		LOGGER.info('join coupon');
 		List<OrderSeatItemDto> items = OrderService.INSTANCE.getOrderSeats(order.id);
 		def param = [];
 		for(x in items) {
